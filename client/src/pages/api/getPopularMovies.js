5ba8b715d38cbb835dc812ee2ad5fcb2dbf92023
star1120eto <etoile.letoile@gmail.com>
2024-02-02 22:33:14 +0900

    feat : エラーをレスポンスに追加

--- client/src/pages/api/getPopularMovies.js
+++ client/src/pages/api/getPopularMovies.js
@@ -1,11 +1,12 @@
 import axios from "axios";
 
-export default async function handler(req, res){
+export default async function handler(req, res) {
     try {
         const response = await axios(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.TMDB_API_KEY}&lamguage=ja-JP`);
         res.status(200).json(response.data);
-        console.log('取得した結果：',response.data);
-    } catch (error) {``
+        console.log('取得した結果：', response.data);
+    } catch (error) {
         console.log(error);
+        res.status(500).json('エラーが発生しました。', error);
     }
 }
