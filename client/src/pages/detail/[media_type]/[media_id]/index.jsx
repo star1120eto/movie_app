08cca37ff6930cfa24692a34f8fada5716e504ca
star1120eto <etoile.letoile@gmail.com>
2024-02-28 21:41:59 +0900

    fix : セミコロン追加

--- client/src/pages/detail/[media_type]/[media_id]/index.jsx
+++ client/src/pages/detail/[media_type]/[media_id]/index.jsx
@@ -61,11 +61,11 @@ export async function getServerSideProps(context) {
     const { media_type, media_id } = context.params
     try {
         const jpResponse = await axios.get(`https://api.themoviedb.org/3/${media_type}/${media_id}?api_key=${process.env.TMDB_API_KEY}&language=ja-JP`);
-        let combinedData = { ...jpResponse.data }
+        let combinedData = { ...jpResponse.data };
 
         if (!jpResponse.data.overview) {
             const enResponse = await axios.get(`https://api.themoviedb.org/3/${media_type}/${media_id}?api_key=${process.env.TMDB_API_KEY}&language=en-US`);
-            combinedData.overview = enResponse.data.overview
+            combinedData.overview = enResponse.data.overview;
         }
 
         return {
